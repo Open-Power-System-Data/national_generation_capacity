22325f6fd03b0303ed138fb2291101634928b8aa
mak <mak@wip.tu-berlin.de>
2019-11-06 16:27:15 +0100

    copy input and download folder into one original_data folder

--- functions/helper_functions.py
+++ functions/helper_functions.py
@@ -3,6 +3,7 @@ import os.path
 import urllib.parse
 import urllib.request
 import zipfile
+import shutil
 from . import logger
 
 
@@ -101,3 +102,19 @@ def checkIfEmptyAndSetDefault(df, technology, default=0):
         return default
     else:
         return sub_df.values[0]
+
+
+def copydir(source, dest):
+    """Copy a directory structure overwriting existing files"""
+    for root, dirs, files in os.walk(source):
+        if not os.path.isdir(root):
+            os.makedirs(root)
+
+        for file in files:
+            rel_path = root.replace(source, '').lstrip(os.sep)
+            dest_path = os.path.join(dest, rel_path)
+
+            if not os.path.isdir(dest_path):
+                os.makedirs(dest_path)
+
+            shutil.copyfile(os.path.join(root, file), os.path.join(dest_path, file))
